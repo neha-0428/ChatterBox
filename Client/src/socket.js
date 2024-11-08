25f2a4c1770fa2e5a4f17c493deb7673b7f197ef
Neha Kadam <nehakadam3007@gmail.com>
2024-11-08 19:24:57 +0530

    Updated for deployment of Client

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -1,7 +1,8 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_API_URL;
+// export const BASE_URL = "http://localhost:5000";
 
 const socket = io(BASE_URL);
 
